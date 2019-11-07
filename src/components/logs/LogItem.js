d24b5e4db12149caf7723b285e6190db5723c006
aa1aac <ach.aayush.111@gmail.com>
2019-11-07 15:09:55 +0545

    log item and preloader

---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Moment from "react-moment";
+
+const LogItem = ({ log }) => {
+  return (
+    <li className="collection-item">
+      <div>
+        <a
+          href="#edit-log-modal"
+          className={`modal-trigger ${
+            log.attention ? "red-text" : "blue-text"
+          }`}
+        >
+          {log.message}
+        </a>
+        <br />
+        <span className="grey-text">
+          hi
+          <span className="black-text">ID # {log.id} </span>
+          last used by <span className="black-text">{log.tech}</span>
+          on <Moment format="MMM DD YYYY, h:mm:ss a ">{log.date}</Moment>
+        </span>
+        <a className="secondary-content" href="#!">
+          <i className="material-icons grey-text">delete</i>
+        </a>
+      </div>
+    </li>
+  );
+};
+
+export default LogItem;
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,35 +1,43 @@
 import React, { useState, useEffect } from "react";
 
+import LogItem from "./LogItem";
+
+import Preloader from "../layout/Preloader";
+
 const Logs = () => {
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
-      getLogs()
-  },[])
+  useEffect(() => {
+    getLogs();
+  }, []);
 
   const getLogs = async () => {
     setLoading(true);
-    const res = await fetch('/logs')
-    const data = await res.json()
+    const res = await fetch("/logs");
+    const data = await res.json();
 
-    setLogs(data)
-    setLoading(false)
+    setLogs(data);
+    setLoading(false);
   };
 
-  if(loading){
-      return <h4> Loading </h4> 
-    }
-  return <div>
-      <ul className="collection-with-header">
-          <li className="collection-header">
-              <h4 className="center"> System Logs</h4>
-          </li>
-          {!loading && logs.length === 0 ? (<p className="center"> No logs to show </p>) : (
-              logs.map(log => <li>{log.message}</li>)
-          )}
+  if (loading) {
+    return <Preloader />;
+  }
+  return (
+    <div>
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4 className="center"> System Logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className="center"> No logs to show </p>
+        ) : (
+          logs.map(log => <LogItem log={log} key={log.id} />)
+        )}
       </ul>
-  </div>;
+    </div>
+  );
 };
 
 export default Logs;
