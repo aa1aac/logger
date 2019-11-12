bb286db1f10c680fbd859076c3782e89fd4becfd
aa1aac <ach.aayush.111@gmail.com>
2019-11-12 20:46:03 +0545

    add logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,29 +1,52 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
 
-export const getLogs = ()=>async dispatch =>{
-    try {
-        setLoading()
+export const getLogs = () => async dispatch => {
+  try {
+    setLoading();
 
-        const res = await fetch('/logs')
-        const data = await res.json()
+    const res = await fetch("/logs");
+    const data = await res.json();
 
-        dispatch({
-            type:GET_LOGS,
-            payload:data
-        })
-    } catch (error) {
-        dispatch({
-            type:LOGS_ERROR,
-            payload:error.response.data
-        })
-    }
-       
-    
-}
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
 
 // set loading to true
-export const setLoading = ()=>{
-    return {
-        type:SET_LOADING,
-    }
-}
+export const setLoading = () => {
+  return {
+    type: SET_LOADING
+  };
+};
+
+export const addLog = log => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
